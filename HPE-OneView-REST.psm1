c4b0fc6b39d0b3c6b55d88640716c1c284108603
i1Smirnov <38904320+i1Smirnov@users.noreply.github.com>
2018-11-09 19:46:00 +0300

    09-11-2018

---
+++ HPE-OneView-REST.psm1
@@ -0,0 +1,88 @@
+﻿function Get-HPEOVAuthToken
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [String] 
+        $OVApplianceIP,
+
+        [Parameter(Mandatory = $true)]
+        [String] 
+        $Domain,
+        
+        [Parameter(Mandatory = $true)]
+        [String] 
+        $UserName,
+        
+        [Parameter(Mandatory = $true)]
+        [String] 
+        $Password
+    )
+    begin
+    {        
+        $body = [Ordered]@{
+            'authLoginDomain' = $Domain.ToUpper()
+            'password'        = $Password
+            'userName'        = $UserName
+            'loginMsgAck'     = 'true'}
+
+        $headers = [Ordered]@{
+            'X-Api-Version' = '600'
+            'Content-Type'  = 'application/json'}
+        
+        $jsonBody    = ConvertTo-Json $body
+        $uri = 'https://' + $OVApplianceIP + '/rest/login-sessions'
+    }
+    process
+    {
+        try
+        {
+            $session = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $jsonBody
+        }
+        catch
+        {
+            $_.Exception.Message
+        }        
+        $session.sessionID
+    }
+}
+
+function Delete-HPEOVAlert
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true,
+        ValueFromPipeline = $true)]
+        [String] 
+        $OVApplianceIP,
+        
+        [Parameter(Mandatory = $true,
+        ValueFromPipeline = $true)]
+        [String] 
+        $Auth,
+        
+        [Parameter(Mandatory = $true,
+        ValueFromPipeline = $true)]
+        [String] $AlertID
+    )
+    begin
+    {
+        $headers = [Ordered]@{
+            'X-Api-Version' = '200'
+            'Auth'  = $Auth}
+
+        $uri = 'https://' + $OVApplianceIP + '/rest/alerts/' + $AlertID + '?force=true'
+    }
+    process
+    {
+        try
+        {
+            Invoke-RestMethod -Method Delete -Uri $uri -Headers $headers | Out-Null
+            Write-Host "Alert $AlertID cleared"
+        }
+        catch
+        {
+            $_.Exception.Message
+        }    
+    }
+}
